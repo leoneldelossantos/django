6846dc85535e1080c38e8fa2a7532446f2f0d6fe
leoneldelossantos <54589912+leoneldelossantos@users.noreply.github.com>
2023-01-19 13:37:19 -0300

    Delete python_34650 directory

--- python_34650/contacts/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    address = forms.CharField(max_length=300)
-    phone_number = forms.CharField(max_length=20)
-    email = forms.EmailField()
-
--- python_34650/contacts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from contacts.views import contacts_list, create_contact
-
-urlpatterns = [
-    path('contacts-list/', contacts_list, name='contacts_list'),
-    path('contacts-create/', create_contact, name='contacts_create'),
-
-]
