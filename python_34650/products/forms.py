bf80e63fe5a2a52376421bf7a6a8d42be1111b98
leoneldelossantos <54589912+leoneldelossantos@users.noreply.github.com>
2023-01-19 13:36:02 -0300

    Delete python_34650 directory

--- python_34650/products/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class ProductForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    price = forms.FloatField()
-    stock = forms.BooleanField(required=False)
