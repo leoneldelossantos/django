1881273a79fc68388d5ac547d8fb4a43e33c95bf
leoneldelossantos <54589912+leoneldelossantos@users.noreply.github.com>
2023-02-02 20:51:22 -0300

    Delete python_34650 directory

--- python_34650/contacts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from contacts.views import contacts_list, create_contact, update_contact, ContactDeleteView
-
-urlpatterns = [
-    path('contacts-list/', contacts_list, name='contacts_list'),
-    path('contacts-create/', create_contact, name='contacts_create'),
-    path('update-contact/<int:pk>/', update_contact, name='update_contact'),
-    path('delete-contact/<int:pk>/', ContactDeleteView.as_view(), name='delete_contact'),
-]
